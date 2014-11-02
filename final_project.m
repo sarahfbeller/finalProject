66e7458ad8a4d25d88e23c9b0b66722eab247da7
James Peter Warwick Spicer <jspicer@corn23.stanford.edu>
2014-11-02 15:44:11 -0800

    Howdy

--- final_project.m
+++ final_project.m
@@ -1,9 +1,40 @@
 function final_project
+	clc; clear all;
 
-	input_files = dir('texts');
+	% get the xpath mechanism into the workspace
+	import javax.xml.xpath.*
+	factory = XPathFactory.newInstance;
+	xpath = factory.newXPath;
 
-	for file = input_files'
-		print file;
+
+	input_files = dir('texts/*.xml');
+	% current_dir = pwd;
+
+	for file = input_files(1)
+		% disp(file.name)
+		% file_URL = strcat('file://', current_dir, '/texts/', file.name)
+		
+		file_path = strcat('texts/', file.name);
+		% xDoc = xmlread(fullfile(matlabroot, 'toolbox','matlab','general',file.name));
+		% [pathstr,name,ext] = fileparts(file_path)
+		% xDoc = xmlread(file_URL);
+		xDoc = xmlread(file_path);
+		% % allListitems = xDoc.getElementsByTagName('listitem')
+		% xRoot = xDoc.getDocumentElement;
+		% f = char(xRoot.getTextContent);
+		% xDoc = parseXML(file_path);
+		% fieldnames(xDoc)
+		% xDoc.Children
+		% cellData = struct2cell(xDoc)
+		xmlwrite(xDoc)
+
+
+
+		% compile and evaluate the XPath Expression
+		expression = xpath.compile('TEI.2/text/body');
+		bodyNode = expression.evaluate(xDoc, XPathConstants.NODE);
+		text_body = bodyNode.getTextContent
+
 	end
 
 end
