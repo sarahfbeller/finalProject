f77b0b31cb87d54f9632af8fcf23d5bd385bd72a
James Peter Warwick Spicer <jspicer@corn13.stanford.edu>
2014-11-12 03:05:48 -0800

    so late

--- final_project.m
+++ final_project.m
@@ -6,17 +6,14 @@ function final_project
 	factory = XPathFactory.newInstance;
 	xpath = factory.newXPath;
 
-
 	input_files = dir('texts/*.xml'); % Reads only .xml files in the 'texts' directory
-	% current_dir = pwd;
-
 
 	work_freq_map = containers.Map();
 	word_set = {};
+	authors_list = {};
 
 	for file = input_files(1)%input_files'
-		% disp(file.name)
-
+		authors_list = [authors_list, strtok(file.name,'_')];
 		file_path = strcat('texts/', file.name);
 		xDoc = xmlread(file_path);
 		xmlwrite(xDoc);
@@ -24,15 +21,11 @@ function final_project
 		% compile and evaluate the XPath Expression
 		expression = xpath.compile('TEI.2/text/body');
 		bodyNode = expression.evaluate(xDoc, XPathConstants.NODE);
-		% class(bodyNode.getTextContent)
 		text_body = char(bodyNode.getTextContent); % Returns Matlab string
-		% class(text_body)
 		lines = regexp(text_body, '\n', 'split'); % Splits into lines
-		% class(lines(3))
 
 		word_freq_map = containers.Map();
 		for i = 1:length(lines) % For every line in the work
-			% lines{i}
 			if isempty(lines{i})
 				continue;
 			end
@@ -41,49 +34,47 @@ function final_project
 				continue; % Ignore it
 			else
 				for j = 1:length(line) % For every word in the line
+					if isempty(line{j})
+						continue;
+					end
 					if isKey(word_freq_map, line{j}) % Record in freq map
 						word_freq_map(line{j}) = word_freq_map(line{j}) + 1;
 					else
 						word_freq_map(line{j}) = 1;
+						word_set = [word_set, line{j}];
 					end
-					word_set = [word_set, line{j}];
 				end
 			end
 		end
-		% keys(word_freq_map);
-		% values(word_freq_map)
 		work_freq_map(file.name) = word_freq_map;
 
 	end
 
 	word_set = unique(word_set);
-	word_set = char(word_set);
+	length(word_set)
 
-	big_X = zeros(length(word_set), length(input_files));
+	big_X = zeros(length(keys(work_freq_map)),length(word_set));
 
-	key_set = keys(work_freq_map);
+	works = keys(work_freq_map);
 
-	for i = 1:length(key_set)
-		current_work = work_freq_map(key_set{i});
-		word_keys = keys(current_work);
-		for j = 1:length(word_keys)
-			curr_word = word_keys{j};
-			curr_freq = current_work(curr_word);
-			% index = find([word_set{:}] == curr_word)
-			% class(curr_word)
-			% class(word_set{1})
-			% iscell(curr_word)
-			% iscell(word_set)
-			class(word_set)
-			class(curr_word)
-			% index = find(word_set==curr_word);
-			index = strfind(word_set,curr_word);
-			% index = strcmp(curr_word, word_set);
-			big_X(index,i) = curr_freq;
+	for i = 1:length(works)
+		current_work  = work_freq_map(works{i});
+		words_in_work = keys(current_work);
+		for j = 1:length(words_in_work)
+			index = 0;
+			for k = 1:length(word_set)
+				if strcmp(char(word_set(k)), words_in_work{j})
+					index = k;
+					break;
+				end
+			end
+			big_X(i,index) = current_work(words_in_work{j});
 
 		end
 	end
 
-	% big_X
+	X = big_X;
+	token_list = word_set;
+	labels = authors_list;
 
 end
