f53f194b5625d25a158d6b03311aca93df5b085e
James Peter Warwick Spicer <jspicer@corn34.stanford.edu>
2014-11-26 15:11:09 -0800

    howdy

--- final_project.m
+++ final_project.m
@@ -1,104 +1,148 @@
 function final_project
 	clc; clear all;
-	tic; % Start stopwatch timer
-	% get the xpath mechanism into the workspace
-	import javax.xml.xpath.*
-	factory = XPathFactory.newInstance;
-	xpath = factory.newXPath;
+	tic; 																	% Start stopwatch timer.
+	
+	import javax.xml.xpath.*												% Get the xPath mechanism into the workspace.
+	factory 		 = XPathFactory.newInstance;
+	xpath 			 = factory.newXPath;
 
-	%input_files = dir('texts/*.xml'); % Reads only .xml files in the 'texts' directory
-	input_files = dir('test/*.xml'); % Reads only .xml files in the 'texts' directory
+	input_directory  = 'test/';
+	noun_endings     = {'as' 'ou' 'an' 'a' 'ain' 'ai' 'wn' 'ais' 'hs' 'h' 'hn' 'os' 'on' 'e' 'w' 'oin' 'ous' 'oi' 'ws' 'us' 'uos' 'ui' 'un' 'u' 'ues' 'uwn' 'usi' 'is' 'ews' 'ei' 'in' 'i' 'eis' 'ewn' 'esi' 'us'};
+	verb_endings     = {'w' 'eis' 'ei' 'omen' 'ete' 'ousi' 'ousin' 'wmen' 'hte' 'wsi' 'wsin' 'oimi' 'ois' 'oi' 'oimen' 'oite' 'oien' 'etw' 'ontwn' 'wsan' 'ein' 'wn' 'ousa' 'on' 'as' 'asa' 'an'};
+	%s and then verb ending -> cut s
+	%ignored middle and passive for now
+	%figure out - iota subscript with a, h, q, hs, 
 
-	work_freq_map = containers.Map();
-	word_set = {};
-	authors_list = {};
+	% Order noun and verb endings by size, largest to smallest.
+	[dummy, index]   = sort(cellfun('size', noun_endings, 2), 'descend');
+  	noun_endings     = noun_endings(index); 
+	[dummy, index]   = sort(cellfun('size', verb_endings, 2), 'descend');
+  	verb_endings     = verb_endings(index);
 
-	%for file = input_files(1:3)
+	% word_freq_map  = containers.Map();
+	% work_freq_map  = containers.Map();
+	% word_set       = {};
+
+	token_list       = {};
+	authors_list     = {};
+	X                = zeros(0,0);
+	input_files      = dir(strcat(input_directory, '*.xml')); 				% Read only .xml files.
+	file_num         = 1;
+
 	for file = input_files'
 		authors_list = [authors_list, strtok(file.name,'_')];
-		file_path = strcat('test/', file.name);
-		% disp('Processing ' file.name);
-		xDoc = xmlread(file_path);
+		file_path    = strcat(input_directory, file.name);
+		xDoc 	     = xmlread(file_path);
 		xmlwrite(xDoc);
 
-		% compile and evaluate the XPath Expression
-		expression = xpath.compile('TEI.2/text/body');
-		bodyNode = expression.evaluate(xDoc, XPathConstants.NODE);
-		text_body = char(bodyNode.getTextContent); % Returns Matlab string
-		lines = regexp(text_body, '\n', 'split'); % Splits into lines
+		expression   = xpath.compile('TEI.2/text/body');					% Compile the XPath Expression.
+		bodyNode     = expression.evaluate(xDoc, XPathConstants.NODE);		% Evaluate the XPath Expression.
+		text_body    = char(bodyNode.getTextContent); 						% Returns Matlab string.
+		lines 	     = regexp(text_body, '\n', 'split'); 					% Splits into lines.
+		lines        = regexprep(lines,'[\/\\=|,.:]','');					% Take out accents.
 
-		word_freq_map = containers.Map();
-		for i = 1:length(lines) % For every line in the work
+		[m,n] 		 = size(X);
+		X 			 = [X; zeros(1,n)];
+
+		for i = 1:length(lines) 											% For every line in the work
 			if isempty(lines{i})
 				continue;
 			end
 			line = regexp(lines{i}, ' ', 'split');
-			if ((length(line) == 1) && (line{1}(1) == '*')) % If there's only one word in the line.
+
+			if ((length(line) == 1) && (line{1}(1) == '*'))					% Skip one-word lines (i.e. speakers).
 				continue;
 			else
-				for j = 1:length(line) % For every word in the line
-					
-					% Primitive stemmer
-					if (length(line{j}) > 5) 
-						line{j} = line{j}(1:end-3);
-					end
+				for j = 1:length(line) 										% For every word in the line.
 
-					if isempty(line{j})
+					if (isempty(line{j}) || (line{j}(1) == '*'))			% Skip empty or capital words.
 						continue;
 					end
-					if isKey(word_freq_map, line{j}) % Record in freq map
-						word_freq_map(line{j}) = word_freq_map(line{j}) + 1;
-					else
-						word_freq_map(line{j}) = 1;
-						word_set = [word_set, line{j}];
+											
+					% Primitive stemmer
+					% if (length(line{j}) > 5) 
+					% 	line{j} = line{j}(1:end-3);
+					% end
+
+					% Not-so-primitive stemmer.
+					% Searches for the longest endings first, i.e. 'ousa' not stemmed prematurely to 'ous'.
+					for k = max(length(noun_endings{1}), length(verb_endings{1})) : -1 : min(length(noun_endings{end}), length(verb_endings{end}))
+						if (length(line{j}) > k)
+							if (~isempty(find(strcmp(line{j}(end-k+1:end), verb_endings))) || ~isempty(find(strcmp(line{j}(end-k+1:end), noun_endings))))
+								line{j} = line{j}(1:end-k);
+								break;
+							end
+						end
 					end
+
+					% noun_index = find(strcmp(line{j}, noun_endings));
+					% verb_index = find(strcmp(line{j}, verb_endings));
+					% if (~isempty(noun_index))
+					% 	line{j} = line{j}(1:end-noun_index);
+					% elseif(~isempty(verb_index))
+					% 	line{j} = line{j}(1:end-verb_index);
+					% end
+
+					index = find(strcmp(line{j}, token_list));
+					if isempty(index)
+						token_list = [token_list, line{j}]; 				% Add word to token list
+						X(file_num, size(token_list)) = 1;  		 		% Add '1' to X matrix.
+					else
+						X(file_num, index) = X(file_num, index) + 1; 		% Increment training matrix.
+					end
+
+
+
+					% if isKey(word_freq_map, line{j}) % Record in freq map
+					% 	word_freq_map(line{j}) = word_freq_map(line{j}) + 1;
+					% else
+					% 	word_freq_map(line{j}) = 1;
+					% 	word_set = [word_set, line{j}];
+					% end
 				end
 			end
 		end
-		work_freq_map(file.name) = word_freq_map;
-		disp(['Finished assembling freq map for ' file.name]);
-
+		% work_freq_map(file.name) = word_freq_map;
+		file_num = file_num + 1;
 	end
-	disp('Finished assembling all word frequency maps in ' num2str(toc) ' seconds.');
+	fprintf('Finished populating X in %d minutes and %d seconds.\n',floor(toc/60),round(rem(toc,60)));
 
-	word_set = unique(word_set);
-	disp(['There are ' num2str(length(word_set)) ' total words in this data!']);
+	% word_set = unique(word_set);
+	% disp(['There are ' num2str(length(word_set)) ' total words in this data!']);
 
-	X = zeros(length(keys(work_freq_map)),length(word_set));
+	% X = zeros(length(keys(work_freq_map)),length(word_set));
 
-	works = keys(work_freq_map);
-	for i = 1:length(works)
-		current_work  = work_freq_map(works{i});
-		words_in_work = keys(current_work);
-		for j = 1:length(words_in_work)
-			index = 0;
-			for k = 1:length(word_set)
-				if strcmp(char(word_set(k)), words_in_work{j})
-					index = k;
-					break;
-				end
-			end
-			X(i,index) = current_work(words_in_work{j});
-		end
-	end
-	disp('Finished populating X');
+	% works = keys(work_freq_map);
+	% for i = 1:length(works)
+	% 	current_work  = work_freq_map(works{i});
+	% 	words_in_work = keys(current_work);
+	% 	for j = 1:length(words_in_work)
+	% 		index = 0;
+	% 		for k = 1:length(word_set)
+	% 			if strcmp(char(word_set(k)), words_in_work{j})
+	% 				index = k;
+	% 				break;
+	% 			end
+	% 		end
+	% 		X(i,index) = current_work(words_in_work{j});
+	% 	end
+	% end
 
-	Y = transpose(authors_list);
-	all_y_hat = [];
+	Y 				 = transpose(authors_list);
+	all_y_hat        = [];
 
 	for i = 1:length(Y)
 		if i == 1
-			model = NaiveBayes.fit(X([i+1:end],:),Y([i+1:end],:), 'Distribution', 'mn');
+			model    = NaiveBayes.fit(X([i+1:end],:),Y([i+1:end],:), 'Distribution', 'mn');
 		elseif i == length(Y)
-			model = NaiveBayes.fit(X([1:i-1],:),Y([1:i-1],:), 'Distribution', 'mn');
+			model    = NaiveBayes.fit(X([1:i-1],:),Y([1:i-1],:), 'Distribution', 'mn');
 		else
-			model = NaiveBayes.fit(X([1:i-1,i+1:end],:),Y([1:i-1,i+1:end],:), 'Distribution', 'mn');
+			model    = NaiveBayes.fit(X([1:i-1,i+1:end],:),Y([1:i-1,i+1:end],:), 'Distribution', 'mn');
 		end
-		y_hat = model.predict(X(i,:));
-		all_y_hat  = [all_y_hat, y_hat];
-
+		y_hat        = model.predict(X(i,:));
+		all_y_hat    = [all_y_hat, y_hat];
+		% fprintf('Model predicted %s, Correct label was %s.\n', all_y_hat{i}, Y{i});
 	end
-	disp('Finished training & predicting.');
 
 	mistakes = 0;
 	for i = 1:length(Y)
@@ -107,6 +151,6 @@ function final_project
 		end
 	end
 
-	test_error = mistakes / length(Y)
-	disp('In all, this algorithm took ' num2str(toc)/60 ' minutes to run.');
+	test_error       = mistakes / length(Y)
+    fprintf('Algorithm completed in %d minutes and %d seconds.\n',floor(toc/60),round(rem(toc,60)));
 end
